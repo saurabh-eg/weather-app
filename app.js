73059de65eb440e00503c491334ef1f7f4501f5a
saurabh-eg <fbgsaurabh@gmail.com>
2025-03-31 22:33:45 +0530

    fix(app.js): update paths for public and template directories

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 const port = 80;
 
 // path of public directory
-const staticPath = path.join(__dirname, "../public");
-const templatePath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const staticPath = path.join(__dirname, "./public");
+const templatePath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
 
 app.set("view engine", "hbs");
 app.set("views", templatePath);
